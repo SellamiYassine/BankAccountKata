845fb580d56cb046d8e38d9590ca0268e2b0507e
Sellami Yassine <yessine.sellemi@ensi-uma.tn>
2020-11-04 21:46:30 +0100

    add sample in main method

--- kata/src/main/java/com/kata/KataBankApplication.java
+++ kata/src/main/java/com/kata/KataBankApplication.java
@@ -1,9 +1,22 @@
 package com.kata;
 
+import com.kata.entities.Account;
+
 public class KataBankApplication {
 
     public static void main(String[] args) {
 
+        Account account = new Account(1L,"1");
+        account.deposit(1000);
+        account.withdrawal(200);
+        account.withdrawal(200);
+        account.deposit(100);
+        account.getAccountStatement();
+        try {
+            account.withdrawal(800);
+        } catch (IllegalArgumentException bankAccountException) {
+            System.out.println(bankAccountException.getMessage());
+        }
     }
 
 }
