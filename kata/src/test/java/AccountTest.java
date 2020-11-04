91bc55cca3e2992c04dccc0ee0e511af35a7ca41
Sellami Yassine <yessine.sellemi@ensi-uma.tn>
2020-11-04 18:13:29 +0100

    add first test account creation

--- kata/src/test/java/AccountTest.java
+++ kata/src/test/java/AccountTest.java
@@ -1,3 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class AccountTest {
 
+    private Account account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new Account(1L,"1");
+    }
+
+    @Test
+    void create_account_test() {
+        Assertions.assertTrue(account.getBalance() == 0.0);
+        Assertions.assertTrue(account.getListOperation().size() == 0);
+    }
 }
